db5a432e6470f265561b0814016ba12e06fc25df
Josh <mcnett88@gmail.com>
2016-08-08 11:34:58 -0400

    4 test with negative and 0 all work

--- multiplication/test_multiplication.rb
+++ multiplication/test_multiplication.rb
@@ -7,4 +7,16 @@ class TestMultiplications < Minitest::Test
     	assert_equal(4, my_multiplications(2,2))
     end
 
+    def test_3_multiply_4_equals_12
+    	assert_equal(12, my_multiplications(3,4))
+    end
+
+    def test_0_mulitiply_5_equals_0
+    	assert_equal(0, my_multiplications(0,5))
+    end
+
+    def test_negative_1_mulitply_2_equals_negative_2
+    	assert_equal(-2, my_multiplications(-1,2))
+    end
+
 end
